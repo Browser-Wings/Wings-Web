e53873d720a7dd6875c5109414ba5d950544eda7
박상민 <a01071397193@gmail.com>
2025-04-01 09:47:50 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
-import { defineConfig } from 'vitest/config';
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		paraglide({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	],
-
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+  plugins: [svelte()],
+})
